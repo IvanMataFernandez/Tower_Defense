070b39d7baee48de9ff0dd4d923e8ced21d940bf
Ivan <ivanmatafernandez@gmail.com>
2024-03-06 21:34:31 +0100

    Initial commit

---
+++ Source/Tower_Defense/GameMode_EnPartida.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameMode_EnPartida.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWER_DEFENSE_API AGameMode_EnPartida : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
